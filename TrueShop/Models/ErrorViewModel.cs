d0ef95b2b358d2bb9f2f582ca0845020ae9cf93c
igor <hihi89@bk.ru>
2018-12-17 12:58:24 +0200

    Init

---
+++ TrueShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrueShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
